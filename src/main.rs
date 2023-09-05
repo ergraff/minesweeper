2ec7cd730819756753f66199006f39a23911ef54
Erik <erikgraff39@gmail.com>
2023-09-05 20:26:51 +0200

    Early map type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+const SIZE: usize = 20;
+
+struct Map {
+    map: Vec<Vec<bool>>,
+}
+
+impl Map {
+    fn new() -> Self {
+        Map {
+            map: vec![vec![false; SIZE]; SIZE],
+        }
+    }
+    fn print(&self) {
+        fn line() {
+            for _ in 0..SIZE {
+                print!("+ - ");
+            }
+            print!("+\n");
+        }
+
+        line();
+        for i in 0..SIZE {
+            for j in 0..SIZE {
+                match self.map[i][j] {
+                    false => print!("|   "),
+                    true => print!("| x "),
+                }
+            }
+            print!("|\n");
+            line();
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let map = Map::new();
+    map.print();
 }
